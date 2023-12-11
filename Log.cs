1b2d3ae1df14fc302d32dc772cad0e13be2431aa
Faim.Badgutdinov <Faim.Badgutdinov@DVL-03>
2023-12-11 11:23:39 +0500

    Добавьте файлы проекта.

---
+++ Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork13
+{
+    public class Log
+    {
+        public ObservableCollection<string> logFile = new ObservableCollection<string>();
+
+        /// <summary>
+        /// Add message to log list
+        /// </summary>
+        /// <param name="msg"></param>
+        public void AddToLog(string msg)
+        {
+            logFile.Add(msg);
+        }
+    }
+}
